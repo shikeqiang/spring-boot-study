b21a24cd9a467b539d2b862714aa2283c74692f5
shikeqiang <452522885@qq.com>
2019-07-19 15:41:48 +0800

    初始化项目

---
+++ springboot-study-day/src/test/java/com/skq/springstury/SpringbootStudyDayApplicationTests.java
@@ -0,0 +1,16 @@
+package com.skq.springstury;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringbootStudyDayApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
